51a67ea7baeb6ee131efc4a4744788ee711f8a2d
Nolan Eakins <sneakin@semanticgap.com>
2019-06-01 19:51:00 -0400

    Regen

--- service_worker.js
+++ service_worker.js
@@ -75,6 +75,7 @@ self.addEventListener('fetch', (event) => {
         }).catch((error) => {
           console.log("Error fetching", event.request, error);
         });
+        return response;
       }).catch((error) => {
         return new Response('' + error.code + ': ' + error.message, {
           status: 404,
